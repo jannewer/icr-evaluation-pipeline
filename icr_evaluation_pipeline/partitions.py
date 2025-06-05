5c8beec2462546c30ac29a5ea0776bd748394d0d
Janne Wernecken <dev@wernecken.com>
2025-06-05 17:42:00 +0200

    feat: Add datasets with more than 5% missing values to exclude list

--- icr_evaluation_pipeline/partitions.py
+++ icr_evaluation_pipeline/partitions.py
@@ -4,7 +4,7 @@ from dagster import StaticPartitionsDefinition
 
 benchmark_suite = openml.study.get_suite("OpenML-CC18")
 all_task_ids = np.array(benchmark_suite.tasks).astype("str").tolist()
-task_ids_to_remove = ["2079"]
+task_ids_to_remove = ["2079", "146800", "125920", "14954", "7592", "3021", "29"]
 selected_task_ids = [
     task_id for task_id in all_task_ids if task_id not in task_ids_to_remove
 ]
